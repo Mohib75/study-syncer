2b8dfeaf0fc75801a1887ed909a498f1b50d2364
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-05-14 13:20:16 +0600

    soinner added

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+	return (
+		<div className='flex justify-center items-center w-full h-full'>
+			<span className='loading loading-spinner text-gray-400 w-40'></span>
+		</div>
+	)
+}
+
+export default Spinner
